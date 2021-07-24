04c739db9ac65ef9d160c2ed643342040ef2417b
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-07-24 16:19:58 +0200

    rails new lewagon_aoc -d postgresql --skip-action-cable --skip-action-mailbox --skip-action-mailer --skip-action-text --skip-active-storage --skip-git --skip-keeps --skip-system-test --skip-test --template https://www.railsbytes.com/script/zyvsbW

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}

535b9e4952a518a17baf9479bf08e9c040a2452b
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-07-31 21:11:30 +0200

    Add default color palette

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -5,7 +5,39 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        aoc: {
+          atmospheric: {
+            DEFAULT: '#79A2D8',
+            light: '#CCDBED'
+          },
+          gray: {
+            DEFAULT: '#CCCCCC',
+            dark: '#666666',
+            darker: '#333340',
+            darkest: '#10101A',
+          },
+          green: {
+            DEFAULT: '#009900',
+            light: '#99FF99',
+            flash: '#00CC00'
+          },
+          gold: {
+            DEFAULT: '#FFFF66',
+            light: '#FFFFCC'
+          },
+          silver: '#9999CC'
+        },
+        dark: '#0F0F23',
+        wagon: {
+          red: {
+            DEFAULT: '#FD1015',
+            light: '#FE9092'
+          }
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],

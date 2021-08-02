65e4078c0d2c2cc2949f1b55ea9a72fc75c13f99
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-08-02 20:37:15 +0200

    Improve readability

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,19 +2,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def kitt
     # pp request.env["omniauth.auth"]
     @user = User.from_kitt(request.env["omniauth.auth"])
+    @contact_me = view_context.link_to(
+      "contact Pilou",
+      "https://lewagon-alumni.slack.com/team/URZ0F4TEF",
+      target: :blank,
+      class: "text-white text-shadow-white"
+    ).html_safe
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       flash.notice = "Successfully signed in with Kitt!"
     else
       redirect_to root_path
-      flash.alert = "For an unknown reason, we couldn't store your information in our database. Please #{view_context.link_to("contact Pilou", "https://lewagon-alumni.slack.com/team/URZ0F4TEF", class: "text-aoc-green-flash text-shadow-green", target: :blank)}."
+      flash.alert = "For an unknown reason, we couldn't store your information in our database. Please #{@contact_me}."
     end
 
     def failure
+      reason = request.env["omniauth.auth"]&.error_description
       redirect_to root_path
-      flash.alert = "Failed to sign in with Kitt (Reason: #{request.env["omniauth.auth"]&.error_description || "Unknown"}). If it persists, please #{view_context.link_to("contact Pilou", "https://lewagon-alumni.slack.com/team/URZ0F4TEF", class: "text-aoc-green-flash text-shadow-green", target: :blank)}."
+      flash.alert = "Failed to sign in with Kitt (Reason: #{reason || "Unknown"}). If it persists, please #{@contact_me}."
     end
   end
 end
-

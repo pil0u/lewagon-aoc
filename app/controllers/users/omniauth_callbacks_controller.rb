313413285bdc73758b87b6fa89c614f606caf63d
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-08-02 20:17:04 +0200

    Handle OmniAuth failure cases

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def kitt
-    # puts request.env["omniauth.auth"]
+    # pp request.env["omniauth.auth"]
     @user = User.from_kitt(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      flash.notice = "Successfully logged in with Kitt!"
+      flash.notice = "Successfully signed in with Kitt!"
     else
       redirect_to root_path
-      flash.alert = "Failed to sign in with Kitt."
+      flash.alert = "For an unknown reason, we couldn't store your information in our database. Please #{view_context.link_to("contact Pilou", "https://lewagon-alumni.slack.com/team/URZ0F4TEF", class: "text-aoc-green-flash text-shadow-green", target: :blank)}."
+    end
+
+    def failure
+      redirect_to root_path
+      flash.alert = "Failed to sign in with Kitt (Reason: #{request.env["omniauth.auth"]&.error_description || "Unknown"}). If it persists, please #{view_context.link_to("contact Pilou", "https://lewagon-alumni.slack.com/team/URZ0F4TEF", class: "text-aoc-green-flash text-shadow-green", target: :blank)}."
     end
   end
 end
+

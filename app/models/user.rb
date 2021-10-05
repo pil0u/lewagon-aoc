ab020fc07886edf0049a1771e17299a20d22616c
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-06 01:12:38 +0200

    Add Batch & City to User (can be nil)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
 
   belongs_to :batch
   belongs_to :city
-  has_many :scores
+  has_many :scores, dependent: :destroy
 
   def self.from_kitt(auth)
+    batch = Batch.find_or_create_by(number: auth.info.last_batch_slug)
+    city = City.find_or_create_by(name: auth.info.cities.last)
+
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.username = auth.info.github_nickname
+      user.batch = batch
+      user.city = city
     end
   end
 end

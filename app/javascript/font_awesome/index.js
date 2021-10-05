92932ad9d0bb916104c763093d68ef1c06a7f88f
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-05 22:49:17 +0200

    Add FontAwesome

---
+++ app/javascript/font_awesome/index.js
@@ -0,0 +1,18 @@
+import { dom, library } from "@fortawesome/fontawesome-svg-core"
+
+/*
+ * Available icons librairies:
+ *  "@fortawesome/free-brands-svg-icons"
+ *  "@fortawesome/free-regular-svg-icons"
+ *  "@fortawesome/free-solid-svg-icons"
+ */
+
+import {
+  faTimes
+} from "@fortawesome/free-solid-svg-icons"
+
+library.add(
+  faTimes
+)
+
+dom.watch()

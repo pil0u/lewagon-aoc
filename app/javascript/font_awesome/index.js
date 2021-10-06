34a60d1998788e4ebc4a31025a730500ed51b674
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-06 02:03:03 +0200

    Add a footer

--- app/javascript/font_awesome/index.js
+++ app/javascript/font_awesome/index.js
@@ -8,11 +8,16 @@ import { dom, library } from "@fortawesome/fontawesome-svg-core"
  */
 
 import {
+  faGithub
+} from "@fortawesome/free-brands-svg-icons"
+
+import {
   faPowerOff,
   faTimes
 } from "@fortawesome/free-solid-svg-icons"
 
 library.add(
+  faGithub,
   faPowerOff,
   faTimes
 )

0dbcd92acd0dabdf3e37e4d737cd5f21f2371f26
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-06 02:01:22 +0200

    Remove flashes fanciness

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,23 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect () {
-    this.element.setAttribute(
-      "style",
-      `transition: 1s; transform:translate(-30.5rem, 0);`
-    )
-
     setTimeout(() => {
       this.close()
     }, 4000)
   }
 
   close () {
-    this.element.setAttribute(
-      "style",
-      `transition: 1s; transform:translate(-2rem, 0);`
-    )
-    setTimeout(() => {
-      this.element.remove()
-    }, 1000)
+    this.element.remove()
   }
 }

adc1d838666028c99dfadf96b8a3f1cf627b7719
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-07 01:49:20 +0200

    Adjust CSS classes names

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
       './app/javascript/**/*.js'
     ],
     safelist: [
-      'underline'
+      'underline',
+      'text-aoc-atmospheric',
+      'text-aoc-green',
+      'text-wagon-red-light',
+      'text-xl'
     ]
   },
   theme: {

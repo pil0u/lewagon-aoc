d8a8a5b4294fcef9735d64f4aa1f320c5b758edc
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-07 17:34:36 +0200

    Add index for City names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_194129) do
+ActiveRecord::Schema.define(version: 2021_10_07_153221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_194129) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_cities_on_name", unique: true
   end
 
   create_table "scores", force: :cascade do |t|

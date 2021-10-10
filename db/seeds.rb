0dae3b35f4e674497848a6c2730fb2b1604004d0
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-10 21:56:20 +0200

    Add users to seed in development (for testing purposes)

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,3 +57,35 @@ end
 
 City.create!(cities)
 Rails.logger.info "✔ Cities initialized"
+
+# Initialize some users in development
+if Rails.env.development?
+  User.create!([
+                 {
+                   username: "test_1",
+                   batch: Batch.create!(number: 343),
+                   aoc_id: 151_323
+                 },
+                 {
+                   username: "test_2",
+                   batch: Batch.create!(number: 454),
+                   aoc_id: 1_095_582
+                 },
+                 {
+                   username: "test_3",
+                   batch: Batch.create!(number: 123),
+                   aoc_id: 1_222_761
+                 },
+                 {
+                   username: "test_4",
+                   batch: Batch.find_by(number: 123),
+                   aoc_id: 1_237_086
+                 },
+                 {
+                   username: "test_5",
+                   batch: Batch.find_by(number: 123),
+                   aoc_id: 1_258_899
+                 }
+               ])
+  Rails.logger.info "✔ Users initialized"
+end

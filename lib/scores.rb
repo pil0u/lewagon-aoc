703bd3ae043a0e8827ac472dce85e56dc3e07d7c
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-10 22:12:10 +0200

    Add generic query to calculate scores

--- lib/scores.rb
+++ lib/scores.rb
@@ -12,4 +12,29 @@ module Scores
       Rails.logger.info "Nothing to do!"
     end
   end
+
+  def self.compute_for(table_name)
+    Rails.logger.info "  Calculating scores for table '#{table_name}'..."
+
+    # TODO: avoid SQL injections?
+    query = """
+      with
+
+      _compute as (
+        select
+          id,
+          dense_rank() over (partition by day, challenge order by completion_unix_time desc) as score
+        from #{table_name}
+      )
+
+      update #{table_name}
+      set
+        updated_at = current_timestamp,
+        score = _compute.score
+      from _compute
+      where #{table_name}.id = _compute.id
+    """
+
+    ActiveRecord::Base.connection.exec_update(query, "compute_#{table_name}")
+  end
 end

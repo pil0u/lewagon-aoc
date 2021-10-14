18da76d905b7628fd1db3a703518a25c0215aef5
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-14 19:53:10 +0200

    Design Dashboard page when synced with AoC: dynamic refresh timers, featured dashboard tiles (link to current puzzle, ranks), visual advent calendar with different color status

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,41 @@
 # frozen_string_literal: true
 
+require 'help'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
-  before_action :find_status, only: %i[dashboard]
+
+  MAGIC_DAYS = [17, 24, 1, 8, 15, 23, 5, 7, 14, 16, 4, 6, 13, 20, 22, 10, 12, 19, 21, 3, 11, 18, 25, 2, 9].freeze
 
   def home; end
 
   def about; end
 
   def dashboard
-    @status_css = {
-      "text-wagon-red-light": @status == "KO",
-      "text-aoc-atmospheric": @status == "pending",
-      "text-aoc-green": @status == "OK"
-    }
+    @status = current_user.status
+
+    @now = Time.now.getlocal("-05:00")
+    @day_of_december = @now.day if @now.month == 12
+    @last_api_fetch = State.first.last_api_fetch_end
+    @next_puzzle_time = @now.month == 12 ? (@now + 1.day).midnight : Time.new(2021, 12, 1, 0, 0, 0, "-05:00")
+
+    sorted_users = Score.group(:user_id).sum(:score_solo).sort_by { |_k, v| -v }
+    @my_score = current_user.scores.sum(:score_solo)
+    @my_rank = sorted_users.index([current_user.id, @my_score])&+1 
+
+    @sorted_batches = Score.includes(user: :batch).group(:batch_id).sum(:score_in_batch).except(nil).sort_by { |_k, v| -v }
+    @my_batch_score = current_user.batch.scores.sum(:score_in_batch)
+    @my_batch_rank = @sorted_batches.index([current_user.batch.id, @my_batch_score])&+1
+
+    @sorted_cities = Score.includes(user: :city).group(:city_id).sum(:score_in_city).except(nil).sort_by { |_k, v| -v }
+    @my_city_score = current_user.city.scores.sum(:score_in_city)
+    @my_city_rank = @sorted_cities.index([current_user.city.id, @my_city_score])&+1
+
+    @my_score_in_batch = current_user.scores.sum(:score_in_batch)
+    @my_score_in_city = current_user.scores.sum(:score_in_city)
+
+    @days = MAGIC_DAYS
   end
 
   def scoreboard; end
-
-  private
-
-  def find_status
-    @status = current_user.status
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def day_css(day, day_of_december)
+    # TODO: eager load all scores
+    parts_solved = Score.where(user: current_user, day: day).count
+
+    {
+      "text-aoc-gray-darker": day_of_december.nil? || day > day_of_december,
+      "text-aoc-gray-dark": parts_solved == 0,
+      "text-aoc-silver": parts_solved == 1,
+      "text-aoc-gold": parts_solved == 2
+    }
+  end
+
+  def status_css(status)
+    {
+      "text-wagon-red-light": status == "KO",
+      "text-aoc-atmospheric": status == "pending",
+      "text-aoc-green": status == "OK"
+    }
+  end
 end

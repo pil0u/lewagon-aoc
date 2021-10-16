2b46fc4fd9c26c6f9d49b6317908e42e00deba13
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-16 11:17:50 +0200

    City information is unreliable, definitely not using it

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,10 @@ class User < ApplicationRecord
 
   def self.from_kitt(auth)
     batch = Batch.find_or_create_by(number: auth.info.last_batch_slug)
-    # city = City.find_or_create_by(name: auth.info.cities.last || "")
 
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.username = auth.info.github_nickname
       user.batch = batch
-      # user.city = city
     end
   end
 

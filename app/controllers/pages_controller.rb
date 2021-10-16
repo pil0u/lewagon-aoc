6c9512a11428c0ede6d929c58509925263e18d13
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-10-16 10:59:50 +0200

    Fix safe navigation operator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,15 +23,15 @@ class PagesController < ApplicationController
 
     sorted_users = Score.group(:user_id).sum(:score_solo).sort_by { |_k, v| -v }
     @my_score = current_user.scores.sum(:score_solo)
-    @my_rank = sorted_users.index([current_user.id, @my_score])&+1 
+    @my_rank = sorted_users.index([current_user.id, @my_score])&.+1 
 
     @sorted_batches = Score.includes(user: :batch).group(:batch_id).sum(:score_in_batch).except(nil).sort_by { |_k, v| -v }
     @my_batch_score = current_user.batch.scores.sum(:score_in_batch)
-    @my_batch_rank = @sorted_batches.index([current_user.batch.id, @my_batch_score])&+1
+    @my_batch_rank = @sorted_batches.index([current_user.batch.id, @my_batch_score])&.+1
 
     @sorted_cities = Score.includes(user: :city).group(:city_id).sum(:score_in_city).except(nil).sort_by { |_k, v| -v }
     @my_city_score = current_user.city.scores.sum(:score_in_city)
-    @my_city_rank = @sorted_cities.index([current_user.city.id, @my_city_score])&+1
+    @my_city_rank = @sorted_cities.index([current_user.city.id, @my_city_score])&.+1
 
     @my_score_in_batch = current_user.scores.sum(:score_in_batch)
     @my_score_in_city = current_user.scores.sum(:score_in_city)

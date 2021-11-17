12674b6aa7beb197dbe969a4061d6d6b26ccbd2f
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-11-17 20:59:51 +0100

    Naming

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   MAGIC_DAYS = [17, 24, 1, 8, 15, 23, 5, 7, 14, 16, 4, 6, 13, 20, 22, 10, 12, 19, 21, 3, 11, 18, 25, 2, 9].freeze
 
   def home
-    @user_count = User.count
+    @total_sign_ins = User.count
   end
 
   def about

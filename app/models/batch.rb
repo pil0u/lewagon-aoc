37fff8a7da32ded40b506c28ff242ef4c2bb5fe4
pil0u <pierreloic.pichon@gmail.com>
2021-12-01 06:32:53 +0100

    Add a minimum number of contributors taken into account for batch score calculations (#33)
    The minimum is set to `3`

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -6,7 +6,9 @@ class Batch < ApplicationRecord
   has_many :users, dependent: :nullify
   has_many :scores, through: :users
 
+  MINIMUM_CONTRIBUTORS = 3
+
   def self.max_contributors
-    Help.median(User.synced.group(:batch_id).count.except(nil).values) || 1
+    [Help.median(User.synced.group(:batch_id).count.except(nil).values) || 1, MINIMUM_CONTRIBUTORS].max
   end
 end

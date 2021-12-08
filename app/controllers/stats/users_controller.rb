15710a818ece5534b7c605267c87b551a0a5bdc8
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-12-08 12:04:31 +0100

    Design User stats page, inspired by the dashboard and AoC's Personal Stats page. Add ranks for batch & city, distinguish silver & gold stars

--- app/controllers/stats/users_controller.rb
+++ app/controllers/stats/users_controller.rb
@@ -3,17 +3,29 @@
 module Stats
   class UsersController < ApplicationController
     before_action :set_user, only: [:show]
+
     def show
-      @total_stars = @user.completions.actual.count
+      @gold_stars = @user.completions.actual.where(challenge: 2).count
+      @silver_stars = @user.completions.actual.where(challenge: 1).count - @gold_stars
+
       @score = @user.score.in_contest.to_i
+      @rank = @user.rank.in_contest
+      @total_users = User.synced.count
+
       if @user.batch
         @batch_score = @user.batch.batch_score.in_contest.to_i
+        @batch_rank = @user.batch.batch_score.rank
+        @total_batches = BatchScore.count
         @batch_contribution = @user.batch_contributions.sum(:points).to_i
       end
+
       if @user.city
         @city_score = @user.city.city_score.in_contest.to_i
+        @city_rank = @user.city.city_score.rank
+        @total_cities = CityScore.count
         @city_contribution = @user.city_contributions.sum(:points).to_i
       end
+
       @completions = @user.completions.actual.left_joins(:point_value, :completion_rank)
                           .select(
                             :day,

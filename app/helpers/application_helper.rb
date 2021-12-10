3091d29e050c4f69a9d33fa93cb3e6ae78d97f7e
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-12-10 13:19:42 +0100

    Remove depreciation warning (the 'in' parameter seems useless for the calculation) and switch DateTime to Time for publication_time() helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,12 +35,12 @@ module ApplicationHelper
     }
   end
 
-  def publication_datetime(day)
-    DateTime.new(ENV["EVENT_YEAR"].to_i, 12, day, 0, 0, 0, "UTC-5")
+  def publication_time(day)
+    Time.new(ENV["EVENT_YEAR"].to_i, 12, day, 0, 0, 0, "-05:00")
   end
 
   def duration_as_chrono(date, since:)
-    date = Time.zone.at(date, in: "+00:00") if date.is_a?(String) || date.is_a?(Numeric)
+    date = Time.zone.at(date) if date.is_a?(String) || date.is_a?(Numeric)
     duration = ActiveSupport::Duration.build(date - since)
     if duration < 1.day
       parts = duration.parts

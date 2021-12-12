8ab020b8fb51e40b3bf69d8f646be2a20375ee55
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2021-12-12 08:49:41 +0100

    Add the number of contributors per challenge on the city page

--- app/controllers/stats/cities_controller.rb
+++ app/controllers/stats/cities_controller.rb
@@ -8,6 +8,13 @@ module Stats
       @city_mates = @city.users
                          .left_joins(:batch).joins(:score, :rank).preload(:score, :rank, :batch)
                          .order("ranks.in_city")
+
+      contributors_per_challenge = Completion.actual.left_joins(user: :city).where("city_id = ?", @city.id).group(:day, :challenge).count.to_h
+      @daily_contributors = contributors_per_challenge.group_by { |key, _l| key.first }.transform_values { |contributors_counts| contributors_counts.map(&:last) }
+
+      @latest_day = Aoc.in_progress? ? Time.now.getlocal("-05:00").day : 25
+
+      @max_contributors = City.max_contributors
     end
 
     private

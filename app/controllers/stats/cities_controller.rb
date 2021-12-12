e8e05f59723218fbbd767b81f299d1b0ebfca5c9
Jérémie <jeremie.bonal@gmail.com>
2021-12-12 05:19:33 +0100

    Fix cities#show

--- app/controllers/stats/cities_controller.rb
+++ app/controllers/stats/cities_controller.rb
@@ -6,8 +6,9 @@ module Stats
 
     def show
       @city_mates = @city.users
-                         .left_joins(:batch).joins(:score, :rank).preload(:score, :rank, :batch)
-                         .order("ranks.in_city")
+                         .left_joins(:batch).joins(:city_contributions, :score)
+                         .preload(:score, :batch, :city_contributions)
+                         .order("rank_in_city").uniq
     end
 
     private

1096cdbfd72075466de1e3da839013819db508f9
Jérémie <jeremie.bonal@gmail.com>
2021-12-16 09:07:55 +0100

    Add remaining_contributions to views.

--- db/views/batch_points_v03.sql
+++ db/views/batch_points_v03.sql
@@ -7,7 +7,8 @@ SELECT
   rank() OVER (PARTITION BY day, challenge ORDER BY SUM(in_contest) DESC) AS rank_in_contest,
 
   COUNT(*) FILTER (WHERE participated) AS participating_users,
-  COUNT(*) FILTER (WHERE participated) >= (VALUES (max_allowed_contributors_in_batch())) AS complete
+  COUNT(*) FILTER (WHERE participated) >= (VALUES (max_allowed_contributors_in_batch())) AS complete,
+  (VALUES (max_allowed_contributors_in_batch())) - COUNT(*) FILTER (WHERE participated) AS remaining_contributions
 
 FROM batch_contributions AS bc
 GROUP BY batch_id, day, challenge;
--- db/views/city_points_v03.sql
+++ db/views/city_points_v03.sql
@@ -7,7 +7,8 @@ SELECT
   rank() OVER (PARTITION BY day, challenge ORDER BY SUM(in_contest) DESC) AS rank_in_contest,
 
   COUNT(*) FILTER (WHERE participated) AS participating_users,
-  COUNT(*) FILTER (WHERE participated) >= (VALUES (max_allowed_contributors_in_batch())) AS complete
+  COUNT(*) FILTER (WHERE participated) >= (VALUES (max_allowed_contributors_in_city())) AS complete,
+  (VALUES (max_allowed_contributors_in_city())) - COUNT(*) FILTER (WHERE participated) AS remaining_contributions
 
 FROM city_contributions AS cc
 GROUP BY city_id, day, challenge;

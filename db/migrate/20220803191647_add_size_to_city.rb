3514f05ecbc3c74cd2885350bf7196ac6d7669ca
Pierre-Loïc Pichon <pierreloic.pichon+git@gmail.com>
2022-08-04 08:51:36 +0200

    Add size to cities, corresponding to the number of Alumni per city. This is extracted from a file given by Sébastien Saunier and will be updated regularly during the competition

---
+++ db/migrate/20220803191647_add_size_to_city.rb
@@ -0,0 +1,5 @@
+class AddSizeToCity < ActiveRecord::Migration[7.0]
+  def change
+    add_column :cities, :size, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_080054) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_191647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_080054) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "size"
     t.index ["name"], name: "index_cities_on_name", unique: true
   end
 

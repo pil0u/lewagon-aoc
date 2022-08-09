515e3fb08992dc1c68acc53a9495f0143f4aee9f
pil0u <8350914+pil0u@users.noreply.github.com>
2022-08-10 00:20:21 +0200

    Rename component LastDataRefresh to LastDataUpdate

--- app/components/last_data_refresh_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class LastDataRefreshComponent < ApplicationComponent
-  def initialize
-    @now = Time.now.utc
-    @last_refresh = State.first.last_api_fetch_end.utc # TODO: caching?
-  end
-end

dc30d0cf7f2c6f87dcccb73f841e985c88daa4d7
pil0u <8350914+pil0u@users.noreply.github.com>
2022-08-14 22:12:32 +0200

    Make Desvise sign in and sign out work even if JavaScript is disabled in browser by switching from :delete to :get sign out method and making the sign in button work on all browsers

--- app/javascript/controllers/disable_link_controller.js
+++ app/javascript/controllers/disable_link_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   now () {
-    this.element.disabled = true
+    // For some reason, Chrome and Safari do not receive Devise's sign in
+    // get requests if the button is disabled instantly. Thus a timeout.
+    setTimeout(() => {
+      this.element.disabled = true
+    }, 50)
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
-    delete "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+    get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
   end
 
   unauthenticated do

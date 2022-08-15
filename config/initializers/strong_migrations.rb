76160c4921a4024f3e35720ec8fba0d442df0421
pil0u <8350914+pil0u@users.noreply.github.com>
2022-08-15 16:37:43 +0200

    Start strong_migrations analysis from a safe, recent point

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Mark existing migrations as safe
-StrongMigrations.start_after = 0
+StrongMigrations.start_after = 20220101000000
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user

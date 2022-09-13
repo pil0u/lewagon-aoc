99a4c2c014f9c49c58eca48cf03376c82f34cc02
pil0u <8350914+pil0u@users.noreply.github.com>
2022-09-13 22:46:35 +0200

    Remove some of the batch-related old stuff

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -7,10 +7,4 @@ class Batch < ApplicationRecord
   has_many :batch_points # rubocop:disable Rails/HasManyOrHasOneDependent -- this is an SQL view
 
   validates :number, numericality: { in: 1...(2**31), message: "should be between 1 and 2^31" }
-
-  MINIMUM_CONTRIBUTORS = 3
-
-  def self.max_contributors
-    [MINIMUM_CONTRIBUTORS, Help.median(User.synced.group(:batch_id).count.except(nil).values) || 1].max
-  end
 end

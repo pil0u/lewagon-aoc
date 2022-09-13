221867e0ef8e6597c0496bc749bf77d200648892
pil0u <8350914+pil0u@users.noreply.github.com>
2022-09-13 22:49:39 +0200

    Remove some of the batch-related old stuff

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,9 +2,6 @@
 
 class Batch < ApplicationRecord
   has_many :users, dependent: :nullify
-  has_many :completions, through: :users
-  has_one :batch_score # rubocop:disable Rails/HasManyOrHasOneDependent -- this is an SQL view
-  has_many :batch_points # rubocop:disable Rails/HasManyOrHasOneDependent -- this is an SQL view
 
   validates :number, numericality: { in: 1...(2**31), message: "should be between 1 and 2^31" }
 end

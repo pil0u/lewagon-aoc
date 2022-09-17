4912dfeb4cf432fc57659a193c1262150b4e783d
pil0u <8350914+pil0u@users.noreply.github.com>
2022-09-18 00:56:16 +0200

    Fix secret_id generation and generate unique name as well

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -3,13 +3,19 @@
 class Squad < ApplicationRecord
   has_many :users, dependent: :nullify
 
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
-  before_create :generate_secret_id
+  before_validation :generate_secret_id, on: :create
 
   private
 
   def generate_secret_id
-    self.secret_id = rand(100_000..999_999) while Squad.exists?(secret_id:)
+    secret_id = rand(100_000..999_999)
+    while Squad.exists?(secret_id: secret_id)
+      secret_id = rand(100_000..999_999)
+    end
+
+    self.secret_id = secret_id
+    self.name = "Squad #{Squad.any? ? Squad.last.id.next : 1}-#{SecureRandom.hex(4)}"
   end
 end

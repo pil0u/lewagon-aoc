cec2e7b2ce061f29cb8985155e1beabfd7ff4a0f
pil0u <8350914+pil0u@users.noreply.github.com>
2022-10-05 18:18:57 +0200

    Generalise the generated HTML for rank changes

--- app/components/scores/squad_row_component.rb
+++ app/components/scores/squad_row_component.rb
@@ -9,20 +9,6 @@ module Scores
     def initialize(squad:, user:)
       @squad = squad
       @user = user
-
-      set_rank_diff
-    end
-
-    private
-
-    def set_rank_diff
-      @rank_diff = if @squad[:previous_rank] < @squad[:rank]
-                     "<i class=\"rank-down\"></i>".html_safe
-                   elsif @squad[:previous_rank] == @squad[:rank]
-                     "<i class=\"rank-equal\"></i>".html_safe
-                   elsif @squad[:previous_rank] > @squad[:rank]
-                     "<i class=\"rank-up\"></i>".html_safe
-                   end
     end
   end
 end

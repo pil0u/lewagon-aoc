d01ac13b338d79aac145537f17f1c58410919578
pil0u <8350914+pil0u@users.noreply.github.com>
2022-10-09 10:26:29 +0200

    Comment out or rename files with ___ prefix for all files to be deprecated, especially with the new cache approach

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,147 +25,146 @@ class PagesController < ApplicationController
   def code_of_conduct; end
   def faq; end
   def setup; end
+  def stats; end
 
   def welcome
     @total_users = User.count
   end
 
-  private
+  #   ### old
 
-  ### old
+  #   MAGIC_DAYS = [17, 24, 1, 8, 15, 23, 5, 7, 14, 16, 4, 6, 13, 20, 22, 10, 12, 19, 21, 3, 11, 18, 25, 2, 9].freeze
 
-  MAGIC_DAYS = [17, 24, 1, 8, 15, 23, 5, 7, 14, 16, 4, 6, 13, 20, 22, 10, 12, 19, 21, 3, 11, 18, 25, 2, 9].freeze
+  #   def dashboard
+  #     # Time
+  #     @now = Time.now.getlocal("-05:00")
 
-  def dashboard
-    # Time
-    @now = Time.now.getlocal("-05:00")
+  #     now_utc = Time.now.utc
+  #     last_api_fetch_utc = State.last.fetch_api_end.utc
+  #     @time_since_last_api_fetch = helpers.distance_of_time_in_words(last_api_fetch_utc, now_utc)
 
-    now_utc = Time.now.utc
-    last_api_fetch_utc = State.last.fetch_api_end.utc
-    @time_since_last_api_fetch = helpers.distance_of_time_in_words(last_api_fetch_utc, now_utc)
+  #     @estimated_next_api_fetch = if now_utc < last_api_fetch_utc + 10.minutes
+  #                                   helpers.distance_of_time_in_words(last_api_fetch_utc + 10.minutes, now_utc)
+  #                                 else
+  #                                   "soon™"
+  #                                 end
 
-    @estimated_next_api_fetch = if now_utc < last_api_fetch_utc + 10.minutes
-                                  helpers.distance_of_time_in_words(last_api_fetch_utc + 10.minutes, now_utc)
-                                else
-                                  "soon™"
-                                end
+  #     next_puzzle_time = Aoc.next_puzzle_time_from(@now)
+  #     @time_to_next_puzzle = helpers.distance_of_time_in_words(@now, next_puzzle_time)
 
-    next_puzzle_time = Aoc.next_puzzle_time_from(@now)
-    @time_to_next_puzzle = helpers.distance_of_time_in_words(@now, next_puzzle_time)
+  #     # Event
+  #     @aoc_in_progress = Aoc.in_progress?
+  #     @year = ENV.fetch("EVENT_YEAR")
+  #     @current_open_room = ENV.fetch("AOC_ROOMS").split(",").last
+  #     @user_status = current_user.status
 
-    # Event
-    @aoc_in_progress = Aoc.in_progress?
-    @year = ENV.fetch("EVENT_YEAR")
-    @current_open_room = ENV.fetch("AOC_ROOMS").split(",").last
-    @user_status = current_user.status
+  #     # User stats
 
-    # User stats
+  #     ## Individual rank & score
+  #     @user_score = {
+  #       rank: current_user.rank.in_contest,
+  #       score: current_user.score.in_contest.to_i,
+  #       score_in_batch: current_user.batch_contributions.sum(:points),
+  #       score_in_city: current_user.city_contributions.sum(:points)
+  #     }
+  #     @total_users = User.synced.count
 
-    ## Individual rank & score
-    @user_score = {
-      rank: current_user.rank.in_contest,
-      score: current_user.score.in_contest.to_i,
-      score_in_batch: current_user.batch_contributions.sum(:points),
-      score_in_city: current_user.city_contributions.sum(:points)
-    }
-    @total_users = User.synced.count
+  #     ## Batch rank & score
+  #     @user_batch = current_user.batch
 
-    ## Batch rank & score
-    @user_batch = current_user.batch
+  #     if @user_batch
+  #       @user_batch_score = { score: @user_batch.batch_score.in_contest.to_i, rank: @user_batch.batch_score.rank }
+  #       @total_batches = BatchScore.count
+  #     end
 
-    if @user_batch
-      @user_batch_score = { score: @user_batch.batch_score.in_contest.to_i, rank: @user_batch.batch_score.rank }
-      @total_batches = BatchScore.count
-    end
+  #     ## City rank & score
+  #     @user_city = current_user.city
 
-    ## City rank & score
-    @user_city = current_user.city
+  #     if @user_city
+  #       @user_city_score = { score: @user_city.city_score.in_contest.to_i, rank: @user_city.city_score.rank }
+  #       @total_cities = CityScore.count
+  #     end
 
-    if @user_city
-      @user_city_score = { score: @user_city.city_score.in_contest.to_i, rank: @user_city.city_score.rank }
-      @total_cities = CityScore.count
-    end
+  #     # Calendar
+  #     @advent_days = MAGIC_DAYS.map { |advent_day| Time.new(2021, 12, advent_day, 0, 0, 0, "-05:00") }
 
-    # Calendar
-    @advent_days = MAGIC_DAYS.map { |advent_day| Time.new(2021, 12, advent_day, 0, 0, 0, "-05:00") }
+  #     # Today
+  #     @today_challenges = {}
 
-    # Today
-    @today_challenges = {}
+  #     [1, 2].each do |challenge|
+  #       user_solved = current_user.completions.find_by(day: @now.day, challenge:)
 
-    [1, 2].each do |challenge|
-      user_solved = current_user.completions.find_by(day: @now.day, challenge:)
+  #       if user_solved
+  #         @today_challenges[challenge] = [true, user_solved.point_value.in_contest]
+  #       else
+  #         last_solved = Completion.actual.where(day: @now.day, challenge:)
+  #                                 .order(:rank_solo).last
 
-      if user_solved
-        @today_challenges[challenge] = [true, user_solved.point_value.in_contest]
-      else
-        last_solved = Completion.actual.where(day: @now.day, challenge:)
-                                .order(:rank_solo).last
+  #         challenge_score = last_solved ? last_solved.point_value.in_contest - 1 : User.synced.count
+  #         @today_challenges[challenge] = [false, challenge_score]
+  #       end
+  #     end
+  #   end
 
-        challenge_score = last_solved ? last_solved.point_value.in_contest - 1 : User.synced.count
-        @today_challenges[challenge] = [false, challenge_score]
-      end
-    end
-  end
+  #   def scoreboard
+  #     @anchors = {
+  #       "#cities-scoreboard": "Cities scoreboard",
+  #       "#user-city-rank": current_user.city&.name,
+  #       "#batches-scoreboard": "Batches scoreboard",
+  #       "#user-batch-rank": ("Batch ##{current_user.batch.number}" if current_user.batch),
+  #       "#solo-scoreboard": "Solo scoreboard",
+  #       "#user-rank": current_user.username
+  #     }.compact
 
-  def scoreboard
-    @anchors = {
-      "#cities-scoreboard": "Cities scoreboard",
-      "#user-city-rank": current_user.city&.name,
-      "#batches-scoreboard": "Batches scoreboard",
-      "#user-batch-rank": ("Batch ##{current_user.batch.number}" if current_user.batch),
-      "#solo-scoreboard": "Solo scoreboard",
-      "#user-rank": current_user.username
-    }.compact
+  #     @ranked_cities = CityScore.joins(:city).left_joins(city: { users: :score }).where("users.synced")
+  #                               .order(:rank, "cities.name").distinct
+  #                               .select("cities.name AS city_name",
+  #                                       Arel.sql("COUNT(*) OVER (PARTITION BY cities.id) AS city_n_users"),
+  #                                       Arel.sql("AVG(scores.in_contest) OVER (PARTITION BY cities.id) AS score_average"),
+  #                                       "city_scores.in_contest AS city_score",
+  #                                       "city_scores.rank AS city_rank")
+  #                               .map { |row| row.attributes.symbolize_keys }
+  #                               .reject { |h| h[:city_name].nil? }
+  #                               .each { |h| h[:city_score] = h[:city_score].to_i }
+  #                               .each { |h| h[:score_average] = h[:score_average].ceil }
+  #     @max_city_contributors = City.max_contributors
 
-    @ranked_cities = CityScore.joins(:city).left_joins(city: { users: :score }).where("users.synced")
-                              .order(:rank, "cities.name").distinct
-                              .select("cities.name AS city_name",
-                                      Arel.sql("COUNT(*) OVER (PARTITION BY cities.id) AS city_n_users"),
-                                      Arel.sql("AVG(scores.in_contest) OVER (PARTITION BY cities.id) AS score_average"),
-                                      "city_scores.in_contest AS city_score",
-                                      "city_scores.rank AS city_rank")
-                              .map { |row| row.attributes.symbolize_keys }
-                              .reject { |h| h[:city_name].nil? }
-                              .each { |h| h[:city_score] = h[:city_score].to_i }
-                              .each { |h| h[:score_average] = h[:score_average].ceil }
-    @max_city_contributors = City.max_contributors
+  #     @ranked_batches = BatchScore.joins(:batch).left_joins(batch: { users: :score }).where("users.synced")
+  #                                 .order(:rank, "batches.number": :desc).distinct
+  #                                 .select("batches.number AS batch_number",
+  #                                         Arel.sql("COUNT(*) OVER (PARTITION BY batches.id) AS batch_n_users"),
+  #                                         Arel.sql("AVG(scores.in_contest) OVER (PARTITION BY batches.id) AS score_average"),
+  #                                         "batch_scores.in_contest AS batch_score",
+  #                                         "batch_scores.rank AS batch_rank")
+  #                                 .map { |row| row.attributes.symbolize_keys }
+  #                                 .reject { |h| h[:batch_number].nil? }
+  #                                 .each { |h| h[:batch_score] = h[:batch_score].to_i }
+  #                                 .each { |h| h[:score_average] = h[:score_average].ceil }
+  #     @max_batch_contributors = Batch.max_contributors
 
-    @ranked_batches = BatchScore.joins(:batch).left_joins(batch: { users: :score }).where("users.synced")
-                                .order(:rank, "batches.number": :desc).distinct
-                                .select("batches.number AS batch_number",
-                                        Arel.sql("COUNT(*) OVER (PARTITION BY batches.id) AS batch_n_users"),
-                                        Arel.sql("AVG(scores.in_contest) OVER (PARTITION BY batches.id) AS score_average"),
-                                        "batch_scores.in_contest AS batch_score",
-                                        "batch_scores.rank AS batch_rank")
-                                .map { |row| row.attributes.symbolize_keys }
-                                .reject { |h| h[:batch_number].nil? }
-                                .each { |h| h[:batch_score] = h[:batch_score].to_i }
-                                .each { |h| h[:score_average] = h[:score_average].ceil }
-    @max_batch_contributors = Batch.max_contributors
+  #     @ranked_users = Score.joins(user: :rank).left_joins(user: %i[batch city]).where("users.synced")
+  #                          .order("ranks.in_contest, users.id DESC")
+  #                          .select("users.uid AS uid",
+  #                                  "users.id AS id",
+  #                                  "users.username AS username",
+  #                                  "batches.number AS batch",
+  #                                  "cities.name AS city", "scores.in_contest AS score_solo", "ranks.in_contest AS rank",
+  #                                  "(SELECT COUNT(*) FROM completions co WHERE co.user_id = users.id AND co.challenge = 1) AS silver_stars",
+  #                                  "(SELECT COUNT(*) FROM completions co WHERE co.user_id = users.id  AND co.challenge = 2) AS gold_stars")
+  #                          .map { |row| row.attributes.symbolize_keys }
+  #                          .each { |h| h[:score_solo] = h[:score_solo].to_i }
+  #   end
 
-    @ranked_users = Score.joins(user: :rank).left_joins(user: %i[batch city]).where("users.synced")
-                         .order("ranks.in_contest, users.id DESC")
-                         .select("users.uid AS uid",
-                                 "users.id AS id",
-                                 "users.username AS username",
-                                 "batches.number AS batch",
-                                 "cities.name AS city", "scores.in_contest AS score_solo", "ranks.in_contest AS rank",
-                                 "(SELECT COUNT(*) FROM completions co WHERE co.user_id = users.id AND co.challenge = 1) AS silver_stars",
-                                 "(SELECT COUNT(*) FROM completions co WHERE co.user_id = users.id  AND co.challenge = 2) AS gold_stars")
-                         .map { |row| row.attributes.symbolize_keys }
-                         .each { |h| h[:score_solo] = h[:score_solo].to_i }
-  end
+  #   def stats
+  #     @gold_stars = Completion.where(challenge: 2).count
+  #     @silver_stars = Completion.where(challenge: 1).count - @gold_stars
+  #     @total_kitt_signups = User.where(provider: "kitt").count
+  #     @total_synced_users = User.synced.count
+  #     @total_participating_users = User.distinct(:id).joins(:completions).merge(Completion.actual).count
 
-  def stats
-    @gold_stars = Completion.where(challenge: 2).count
-    @silver_stars = Completion.where(challenge: 1).count - @gold_stars
-    @total_kitt_signups = User.where(provider: "kitt").count
-    @total_synced_users = User.synced.count
-    @total_participating_users = User.distinct(:id).joins(:completions).merge(Completion.actual).count
-
-    stars_per_challenge = Completion.actual.group(:day, :challenge).count.sort_by(&:first).to_h
-    @stars_per_day = stars_per_challenge.group_by { |key, _l| key.first }.transform_values { |star_counts| star_counts.sort_by(&:first).map(&:last) }
-    # AoC formula for how many users per star
-    @users_per_star = (stars_per_challenge.map(&:last).max.to_f / 40).ceil
-  end
+  #     stars_per_challenge = Completion.actual.group(:day, :challenge).count.sort_by(&:first).to_h
+  #     @stars_per_day = stars_per_challenge.group_by { |key, _l| key.first }.transform_values { |star_counts| star_counts.sort_by(&:first).map(&:last) }
+  #     # AoC formula for how many users per star
+  #     @users_per_star = (stars_per_challenge.map(&:last).max.to_f / 40).ceil
+  #   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  ### old
+  # ### old
 
-  def publication_time(day)
-    Time.new(ENV.fetch("EVENT_YEAR").to_i, 12, day, 0, 0, 0, "-05:00")
-  end
+  # def publication_time(day)
+  #   Time.new(ENV.fetch("EVENT_YEAR").to_i, 12, day, 0, 0, 0, "-05:00")
+  # end
 
-  def duration_as_chrono(date, since:)
-    date = Time.zone.at(date) if date.is_a?(String) || date.is_a?(Numeric)
-    duration = ActiveSupport::Duration.build(date - since)
-    if duration < 1.day
-      parts = duration.parts
-      [parts[:hours] || 0, parts[:minutes] || 0, parts[:seconds] || 0].map { |n| n.to_i.to_s.rjust(2, "0") }.join(":")
-    else
-      ">24h"
-    end
-  end
+  # def duration_as_chrono(date, since:)
+  #   date = Time.zone.at(date) if date.is_a?(String) || date.is_a?(Numeric)
+  #   duration = ActiveSupport::Duration.build(date - since)
+  #   if duration < 1.day
+  #     parts = duration.parts
+  #     [parts[:hours] || 0, parts[:minutes] || 0, parts[:seconds] || 0].map { |n| n.to_i.to_s.rjust(2, "0") }.join(":")
+  #   else
+  #     ">24h"
+  #   end
+  # end
 end

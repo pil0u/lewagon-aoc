0d1be6ecbefe3a0f5b67ce7d5430f0f3bb29ab51
Jérémie <jeremie.bonal@gmail.com>
2022-10-10 17:42:56 +0200

    Remove the useless transactions and adjust the cache_key to the new State db structure

--- app/domains/scores/solo_points.rb
+++ app/domains/scores/solo_points.rb
@@ -7,26 +7,24 @@ module Scores
     private
 
     def cache_key
-      @key ||= State.order(:last_api_fetch_end).last.last_api_fetch_end
+      @key ||= State.order(:fetch_api_end).last.fetch_api_end
     end
 
     RETURNED_ATTRIBUTES = [:score, :user_id, :day, :challenge]
 
     def compute
-      ActiveRecord::Base.transaction do
-        completions = Completion.select(Arel.star, Arel.sql(<<~SQL))
-          CASE
-          WHEN duration <= interval '24 hours'
-            THEN 50
-          WHEN duration <= '48 hours'
-            THEN 50 - (EXTRACT(EPOCH FROM (duration - interval '1 day')) / 3600)::integer
-          ELSE
-            25
-          END AS score
-        SQL
+      completions = Completion.select(Arel.star, Arel.sql(<<~SQL))
+        CASE
+        WHEN duration <= interval '24 hours'
+          THEN 50
+        WHEN duration <= '48 hours'
+          THEN 50 - (EXTRACT(EPOCH FROM (duration - interval '1 day')) / 3600)::integer
+        ELSE
+          25
+        END AS score
+      SQL
 
-        completions.map { |c| c.attributes.symbolize_keys.slice(*RETURNED_ATTRIBUTES) }
-      end
+      completions.map { |c| c.attributes.symbolize_keys.slice(*RETURNED_ATTRIBUTES) }
     end
   end
 end
--- app/domains/scores/solo_scores.rb
+++ app/domains/scores/solo_scores.rb
@@ -7,21 +7,19 @@ module Scores
     private
 
     def cache_key
-      @key ||= State.order(:last_api_fetch_end).last.last_api_fetch_end
+      @key ||= State.order(:fetch_api_end).last.fetch_api_end
     end
 
     RETURNED_ATTRIBUTES = [:score, :user_id]
 
     def compute
-      ActiveRecord::Base.transaction do
-        points = Scores::SoloPoints.get
-        points
-          .group_by { |p| p[:user_id] }
-          .map do |user_id, user_points|
-            total_score = user_points.sum { |u| u[:score] }
-            { user_id: user_id, score: total_score  }
-          end
-      end
+      points = Scores::SoloPoints.get
+      points
+        .group_by { |p| p[:user_id] }
+        .map do |user_id, user_points|
+          total_score = user_points.sum { |u| u[:score] }
+          { user_id: user_id, score: total_score  }
+        end
     end
   end
 end

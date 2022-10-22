edb72c1dc8ba18946e8ad83727156acf5fd09b4c
Jérémie <jeremie.bonal@gmail.com>
2022-10-22 17:37:51 +0200

    Add solid cache invalidation specs

--- spec/domains/scores/squad_scores_spec.rb
+++ spec/domains/scores/squad_scores_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Scores::SquadScores do
   ] }
 
   before do
-    allow(Scores::SquadPoints).to receive(:get).and_return(squad_points)
+    allow(Scores::SquadPoints).to receive(:get).and_return(squad_points).once
   end
 
   it "groups the scores of squad members into the squad score" do
@@ -42,6 +42,69 @@ RSpec.describe Scores::SquadScores do
       it "doesn't do any computation" do
         expect_any_instance_of(described_class).not_to receive(:compute)
         described_class.get
+      end
+
+      context "when AOC state has been refetched in the meantime" do
+        before do
+          create(:state, fetch_api_begin: state.fetch_api_end + 2.seconds)
+          allow(Scores::SquadPoints).to receive(:get).and_return(new_squad_points).once
+        end
+
+        let(:new_squad_points) { [
+          { score: 53, squad_id: 1, day: 1, challenge: 1 },
+          { score: 50, squad_id: 1, day: 1, challenge: 2 },
+          { score: 50, squad_id: 2, day: 1, challenge: 1 },
+          { score: 50, squad_id: 2, day: 1, challenge: 2 },
+        ] }
+
+        it "doesn't provide stale results" do
+          expect(described_class.get).to contain_exactly(
+            { score: 103, squad_id: 1 },
+            { score: 100, squad_id: 2 },
+          )
+        end
+
+        it "recomputes" do
+          expect_any_instance_of(described_class).to receive(:compute).and_call_original
+          described_class.get
+        end
+
+        it "creates new cache records" do
+          expect { described_class.get }.to change { Cache::SquadScore.count }.from(2).to(4)
+        end
+      end
+
+      context "when the users makeup of the squad has changed in the meantime" do
+        before do
+          travel 10.seconds # Specs go too fast, updated_at stays the same otherwise
+          create(:user, squad: squad_3)
+          allow(Scores::SquadPoints).to receive(:get).and_return(new_squad_points).once
+        end
+
+        let(:new_squad_points) { [
+          { score: 53, squad_id: 1, day: 1, challenge: 1 },
+          { score: 25, squad_id: 1, day: 1, challenge: 2 },
+          { score: 50, squad_id: 2, day: 1, challenge: 1 },
+          { score: 50, squad_id: 2, day: 1, challenge: 2 },
+          { score: 50, squad_id: 3, day: 1, challenge: 1 },
+        ] }
+
+        it "doesn't provide stale results" do
+          expect(described_class.get).to contain_exactly(
+            { score: 78, squad_id: 1 },
+            { score: 100, squad_id: 2 },
+            { score: 50, squad_id: 3 },
+          )
+        end
+
+        it "recomputes" do
+          expect_any_instance_of(described_class).to receive(:compute).and_call_original
+          described_class.get
+        end
+
+        it "creates new cache records" do
+          expect { described_class.get }.to change { Cache::SquadScore.count }.from(2).to(5)
+        end
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
+  config.include ActiveSupport::Testing::TimeHelpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"

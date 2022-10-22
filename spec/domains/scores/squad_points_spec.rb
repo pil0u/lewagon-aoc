3f45b3e54d15369e1ccefd5051c0ee0cc5bc0f79
Jérémie <jeremie.bonal@gmail.com>
2022-10-22 16:59:03 +0200

    Add spec for SquadPoints

---
+++ spec/domains/scores/squad_points_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Scores::SquadPoints do
+  let!(:state) { create(:state) }
+
+  let!(:squad_1) { create(:squad, id: 1) }
+  let!(:user_1) { create(:user, id: 1, squad: squad_1) }
+  let!(:user_2) { create(:user, id: 2, squad: squad_1) }
+
+  let!(:squad_2) { create(:squad, id: 2) }
+  let!(:user_3) { create(:user, id: 3, squad: squad_2) }
+
+  let!(:squad_3) { create(:squad, id: 3) }
+
+  let(:solo_points) { [
+    { score: 28, user_id: 1, day: 1, challenge: 1 },
+    { score: 25, user_id: 1, day: 1, challenge: 2 },
+
+    { score: 25, user_id: 2, day: 1, challenge: 1 },
+
+    { score: 50, user_id: 3, day: 1, challenge: 1 },
+    { score: 50, user_id: 3, day: 1, challenge: 2 },
+  ] }
+
+  before do
+    allow(Scores::SoloPoints).to receive(:get).and_return(solo_points)
+  end
+
+  it "groups the scores of squad members into the squad score" do
+    expect(described_class.get).to contain_exactly(
+      { score: 53, squad_id: 1, day: 1, challenge: 1 },
+      { score: 25, squad_id: 1, day: 1, challenge: 2 },
+      { score: 50, squad_id: 2, day: 1, challenge: 1 },
+      { score: 50, squad_id: 2, day: 1, challenge: 2 },
+    )
+  end
+
+  describe "caching" do
+    it "creates cache records on first call" do
+      expect { described_class.get }.to change { Cache::SquadPoint.count }.from(0).to(4)
+    end
+
+    context "on second call" do
+      before do
+        described_class.get
+      end
+
+      it "doesn't re-create cache" do
+        expect { described_class.get }.not_to change { Cache::SquadPoint.count }
+      end
+
+      it "doesn't do any computation" do
+        expect_any_instance_of(described_class).not_to receive(:compute)
+        described_class.get
+      end
+    end
+  end
+end

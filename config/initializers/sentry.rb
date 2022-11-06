f0342675c07a737a520072d3be73adb5c186c38a
pil0u <8350914+pil0u@users.noreply.github.com>
2022-11-06 22:21:30 +0100

    Define and set Sentry environment for each deployment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,13 @@ Sentry.init do |config|
   config.dsn = "https://eac0ddb72cec4f11bd78df30df76d742@o4504023997087744.ingest.sentry.io/4504023998988288"
   config.breadcrumbs_logger = %i[monotonic_active_support_logger http_logger]
 
+  # Current environments
+  #   development       default local value
+  #   fly               lewagon-aoc.fly.dev
+  #   fly-pr            lewagon-aoc-pr.fly.dev
+  #   heroku-staging    lewagon-aoc-staging.herokuapp.com
+  #   production        aoc.lewagon.community
+
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.

915ea50b93da1b36ea76ec7b195d6b8591d9a7fe
Pilou <8350914+pil0u@users.noreply.github.com>
2022-11-06 20:48:45 +0100

    Add Sentry logger (#132)
    with basic configuration + user context (id, aoc_id and github_username) (closes #67)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :sentry_set_user
+
+  private
+
+  def sentry_set_user
+    Sentry.set_user(
+      id: current_user&.id,
+      aoc_id: current_user&.aoc_id,
+      github_username: current_user&.github_username
+    )
+  end
 end

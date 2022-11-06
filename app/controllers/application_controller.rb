e5d53a04e50107dffd9b37a6a2d292e4a7c5c3f4
pil0u <8350914+pil0u@users.noreply.github.com>
2022-11-06 20:03:03 +0100

    Add User id, aoc_id and github_username in Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :sentry_set_user
+
+  def sentry_set_user
+    Sentry.set_user(
+      id: current_user&.id,
+      aoc_id: current_user&.aoc_id,
+      github_username: current_user&.github_username
+    )
+  end
 end

4ecbf297f677e7051e7bcc1d164e0f2977844b31
Jérémie <jeremie.bonal@gmail.com>
2022-12-03 09:58:07 +0100

    Add Unlocker mechanism to get achievements

---
+++ app/jobs/achievements/unlock_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Achievements::UnlockJob < ApplicationJob
+  queue_as :default
+
+  def perform(nature, user_id)
+    user = User.find(user_id)
+    unlocker = "Achievements::#{nature.classify}Unlocker".safe_constantize
+    raise "No Achievement Unlocker exist for nature #{nature.inspect}"
+    unlocker.call(user)
+  end
+end

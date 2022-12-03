41fa5f106f34e924462cc745c25deef70c9c3469
Jérémie <jeremie.bonal@gmail.com>
2022-12-03 10:00:05 +0100

    Plug MassUnlocker example achievement on insanity score refresh

--- app/domains/scores/cached_computer.rb
+++ app/domains/scores/cached_computer.rb
@@ -8,16 +8,23 @@ module Scores
 
     private
 
+    # Lifecycle hook
+    def after_compute; end
+
     def cache(model)
       cached_results = model.where(cache_fingerprint: cache_key.to_s)
 
       if cached_results.any?
         attributes = self.class::RETURNED_ATTRIBUTES
+
         cached_results.pluck(*attributes).map { |r| attributes.zip(r).to_h }
       else
         results = yield
         to_cache = results.map { |attrs| attrs.merge(cache_fingerprint: cache_key.to_s) }
         model.insert_all(to_cache) if to_cache.any?
+
+        after_compute # Hook call
+
         results
       end
     end

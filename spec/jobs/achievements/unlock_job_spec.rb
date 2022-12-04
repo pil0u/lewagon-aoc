59e9818851d6d8be5b6c4ca470a8f5f3d4b971d1
Jérémie <jeremie.bonal@gmail.com>
2022-12-04 15:12:33 +0100

    Add spec for UnlockJob

---
+++ spec/jobs/achievements/unlock_job_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Achievements::UnlockJob do
+  let(:user) { create :user }
+
+  before do
+    unlocker = Class.new(Achievements::Unlocker) { def call; end }
+    stub_const('Achievements::MyTestUnlocker', unlocker)
+  end
+
+  it "calls the appropriate Unlocker with the user based on the nature" do
+    expect(Achievements::MyTestUnlocker).to receive(:call).with(user)
+    described_class.perform_now(:my_test, user.id)
+  end
+
+  context "when provided a non-existent user" do
+    it "raises" do
+      expect { described_class.perform_now(:my_test, 404) }.
+        to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  context "when provided a non-existent achievement" do
+    it "raises" do
+      expect { described_class.perform_now(:not_real, user.id) }.
+        to raise_error /No Achievement.*not_real/
+    end
+  end
+end

5b097914fe0ab64fd72efad3e445d30d556f1ce4
Jérémie <jeremie.bonal@gmail.com>
2022-12-04 15:26:01 +0100

    Add spec for Unlocker

---
+++ spec/domains/achievements/unlocker_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Achievements::Unlocker do
+  let(:unlocker) { MyTestUnlocker }
+  let!(:user) { create :user }
+
+  before do
+    stub_const('MyTestUnlocker', Class.new(described_class) { def call; unlock!; end })
+  end
+
+  it "creates an achievement of the correct nature for the user" do
+    expect { unlocker.call(user) }.to change { user.achievements.count }.from(0).to(1)
+  end
+
+  it "knows the nature of the achievement associated to the unlocker" do
+    expect(unlocker.nature).to eq('my_test')
+  end
+
+  context "when the user already has the achievement" do
+    before do
+      user.achievements.create!(nature: 'my_test')
+    end
+
+    it "doesn't perform any check again" do
+      expect_any_instance_of(unlocker).not_to receive(:unlock!)
+      unlocker.call(user)
+    end
+  end
+end

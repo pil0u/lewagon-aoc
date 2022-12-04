be1603536736d9b9d775c03330ba0c3e6ad81067
pil0u <8350914+pil0u@users.noreply.github.com>
2022-12-04 20:17:02 +0100

    Use a dictionnary of (more) languages to display better

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,9 +1,28 @@
 class Snippet < ApplicationRecord
-  LANGUAGES = %w(java javascript python ruby)
+  LANGUAGES = {
+    c:          "C",
+    cpp:        "C++",
+    crystal:    "Crystal",
+    csharp:     "C#",
+    go:         "Go",
+    haskell:    "Haskell",
+    java:       "Java",
+    javascript: "JavaScript",
+    matlab:     "MATLAB",
+    ocaml:      "OCaml",
+    pascal:     "Pascal",
+    php:        "PHP",
+    python:     "Python",
+    r:          "R",
+    ruby:       "Ruby",
+    rust:       "Rust",
+    sql:        "SQL",
+    typescript: "TypeScript"
+  }
 
   belongs_to :user
 
   validates :code, presence: true
-  validates :language, inclusion: { in: LANGUAGES }
+  validates :language, inclusion: { in: LANGUAGES.keys.map(&:to_s) }
   validates :user_id, uniqueness: { scope: [:day, :challenge, :language], message: "can submit only 1 solution per language" }
 end

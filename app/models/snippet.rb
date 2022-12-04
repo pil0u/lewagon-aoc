e8f09d8fb4bc5a356f30dcb5afd9231d076f8d36
pil0u <8350914+pil0u@users.noreply.github.com>
2022-12-04 17:26:12 +0100

    Add logic to sumbit a snippet of solution for a given challenge

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,9 @@
 class Snippet < ApplicationRecord
+  LANGUAGES = %w(java javascript python ruby)
+
   belongs_to :user
+
+  validates :code, presence: true
+  validates :language, inclusion: { in: LANGUAGES }
+  validates :user_id, uniqueness: { scope: [:day, :challenge, :language], message: "can submit only 1 solution per language" }
 end

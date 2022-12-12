8a6a20236fd566474652668836455c21c661dd1a
pil0u <8350914+pil0u@users.noreply.github.com>
2022-12-12 18:46:29 +0100

    Wording

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     # Sort user achievements in the same order as in the YAML definition
     @achievements = @user.achievements
                          .pluck(:nature)
-                         .sort_by { |achievement| Achievement.keys.index(achievement.to_sym) }
+                         .sort_by { |nature| Achievement.keys.index(nature.to_sym) }
 
     @latest_day = Aoc.latest_day
     @daily_completions = Array.new(@latest_day) { [nil, nil] }

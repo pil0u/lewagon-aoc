77d91a8c62148b78995393dad8a66f1850bc7864
Jérémie <jeremie.bonal@gmail.com>
2022-12-13 08:21:14 +0100

    Set up UserDayScores to clean up computations

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -14,20 +14,17 @@ class DaysController < ApplicationController
     @snippets_part_one = Snippet.where(day: @day, challenge: 1).count
     @snippets_part_two = Snippet.where(day: @day, challenge: 2).count
 
-    attributes = %i[user_id username challenge duration]
-    user_completions = daily_completions.pluck(*attributes).map { |completion| attributes.zip(completion).to_h }
-    @users = Scores::SoloPoints.get
-                               .select { |score| score[:day] == @day }
-                               .group_by { |score| score[:user_id] }
-                               .transform_values { |scores| scores.sum { |score| score[:score] } }
-                               .map do |user_id, score|
-      {
-        username: user_completions.find { |completion| completion[:user_id] == user_id }[:username],
-        part_1: user_completions.find { |completion| completion[:user_id] == user_id && completion[:challenge] == 1 }&.dig(:duration),
-        part_2: user_completions.find { |completion| completion[:user_id] == user_id && completion[:challenge] == 2 }&.dig(:duration),
-        score:
-      }
-    end
+    @users = Scores::UserDayScores.get
+      .select { |score| score[:day] == @day }
+      .map do |score|
+        user = User.find(score[:user_id])
+        {
+          score: score[:score],
+          username: user.username,
+          part_1: Completion.find_by(id: score[:part_1_completion_id])&.duration,
+          part_2: Completion.find_by(id: score[:part_2_completion_id])&.duration,
+        }
+      end
 
     @users.sort_by! { |user| [user[:score] * -1, user[:part_2].nil? ? user[:part_1] : user[:part_2], user[:part_1]] }
   end

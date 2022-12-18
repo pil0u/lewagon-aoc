713730a02c04a3e0ecbee631b35cd13d5c0a6e93
pil0u <8350914+pil0u@users.noreply.github.com>
2022-12-18 18:52:46 +0100

    Fix RuboCop issues

--- app/domains/achievements/github_star_mass_unlocker.rb
+++ app/domains/achievements/github_star_mass_unlocker.rb
@@ -3,9 +3,9 @@
 module Achievements
   class GithubStarMassUnlocker < MassUnlocker
     def call
-      uri = URI('https://api.github.com/repos/pil0u/lewagon-aoc/stargazers')
+      uri = URI("https://api.github.com/repos/pil0u/lewagon-aoc/stargazers")
       response = Net::HTTP.get(uri)
-      github_usernames = JSON.parse(response).map { |object| object["login"] }
+      github_usernames = JSON.parse(response).pluck("login")
       eligible_users = User.where(github_username: github_usernames)
 
       unlock_for!(eligible_users)

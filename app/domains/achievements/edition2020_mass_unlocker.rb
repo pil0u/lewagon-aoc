542336a162c28e492fffa1b159d61b565a670e47
pil0u <8350914+pil0u@users.noreply.github.com>
2022-12-18 16:40:32 +0100

    Add the achievement for 2020 puzzle completers

---
+++ app/domains/achievements/edition2020_mass_unlocker.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Achievements
+  class Edition2020MassUnlocker < MassUnlocker
+    def call
+      aoc_ids = [
+        151323, 1095582, 1222761, 1258899, 1259034, 1259062, 1259379, 1266861,
+        1266916, 1267074, 1267450, 1276674, 1276836, 1282141, 1283809, 1289187,
+        1289409, 1291741, 1292231, 1301345, 1304972, 1330508, 1331786, 1349046
+      ]
+      eligible_users = User.where(aoc_id: aoc_ids)
+
+      unlock_for!(eligible_users)
+    end
+  end
+end
--- app/domains/completions/fetcher.rb
+++ app/domains/completions/fetcher.rb
@@ -119,6 +119,7 @@ module Completions
 
     def unlock_achievements
       Achievements::MassUnlockJob.perform_later(:setup_complete)
+      Achievements::MassUnlockJob.perform_later(:edition2020)
       Achievements::MassUnlockJob.perform_later(:stars1)
       Achievements::MassUnlockJob.perform_later(:stars5)
       Achievements::MassUnlockJob.perform_later(:stars11)

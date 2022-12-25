4fd8b5261c9b66294bff1f9a0f6e1f3e88b1e902
Jérémie <jeremie.bonal@gmail.com>
2022-12-25 20:26:20 +0100

    Tier-breakerer for SoloScores since otherwise we have true equalities

--- app/domains/ranks/solo_scores.rb
+++ app/domains/ranks/solo_scores.rb
@@ -5,6 +5,7 @@ module Ranks
     def initialize(scores)
       super
       # Index for o(1) fetch later on
+      @users = User.where(id: scores.pluck(:user_id)).index_by(&:id)
       @completions = Completion.where(user_id: scores.pluck(:user_id)).group_by(&:user_id)
     end
 
@@ -16,8 +17,11 @@ module Ranks
       completions = @completions.fetch(score[:user_id], [])
       [
         score[:score],
+        # Priorizing the display of non-insanity players (since those have their own leaderboard)
+        @users[score[:user_id]].entered_hardcore ? 0 : 1,
         completions.count { |c| c.duration < 24.hours },
         completions.count { |c| c.duration < 48.hours },
+        -completions.sum(&:duration)
       ]
     end
   end

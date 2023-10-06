900d7c6861037400c897b7ea73b198069aa500bb
pil0u <8350914+pil0u@users.noreply.github.com>
2023-10-06 11:51:33 +0200

    Remove or rewrite migrations that contained scenic (SQL views) related code that is deprecated but breaks deployment

--- db/migrate/20211127020610_create_completion_ranks.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CreateCompletionRanks < ActiveRecord::Migration[6.1]
-  def change
-    create_view :completion_ranks, materialized: true
-    add_index :completion_ranks, :completion_id, unique: true
-    add_index :completion_ranks, :in_batch
-  end
-end
--- db/migrate/20211127020725_create_point_values_view.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CreatePointValuesView < ActiveRecord::Migration[6.1]
-  def change
-    create_view :point_values, materialized: true
-    add_index :point_values, :completion_id, unique: true
-  end
-end

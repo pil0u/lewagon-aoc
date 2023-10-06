45f0b9a0a96c18b867e10ad61bb6166fe903e7d2
pil0u <8350914+pil0u@users.noreply.github.com>
2023-10-06 13:51:59 +0200

    Delete SQL views

--- db/views/completion_ranks_v01.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  co.id AS completion_id,
-  dense_rank() OVER (PARTITION BY             co.day, co.challenge ORDER BY co.completion_unix_time ASC) AS in_contest,
-  dense_rank() OVER (PARTITION BY u.batch_id, co.day, co.challenge ORDER BY co.completion_unix_time ASC) AS in_batch,
-  dense_rank() OVER (PARTITION BY u.city_id,  co.day, co.challenge ORDER BY co.completion_unix_time ASC) AS in_city
-FROM completions co
-LEFT JOIN users u
-ON co.user_id = u.id
-WHERE co.completion_unix_time IS NOT NULL;

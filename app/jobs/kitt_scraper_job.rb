57f822bddc0061234f886afe2fc3e92973a053d5
Louis Ramos <louisramosdev@gmail.com>
2023-10-15 11:11:34 +0200

    Refactor KittScrapperJob with CSV headers

--- app/jobs/kitt_scraper_job.rb
+++ app/jobs/kitt_scraper_job.rb
@@ -6,11 +6,9 @@ require "net/http"
 class KittScraperJob < ApplicationJob
   CSV_PATH = "db/static/kitt_alumni.csv"
 
-  # User: [uid, batch_number, batch_year, city_name]
   def perform(scrape_kitt: true, upsert_data: true)
-    @users = CSV.table(CSV_PATH, headers: false).to_a # Array<User>
     perform_scrape_kitt if scrape_kitt
-    perform_update_batches if upsert_data
+    perform_upsert_data if upsert_data
   end
 
   private
@@ -33,52 +31,50 @@ class KittScraperJob < ApplicationJob
   end
 
   def perform_scrape_kitt
-    @uids = @users.map.with_object(Set.new) { |user, set| set.add(user[0]) }
+    @known_uids = CSV.read(CSV_PATH, headers: true).pluck("uid").to_set(&:to_i)
     @new_users = []
-    @page_num = 1
+    page = 1
 
     loop do
-      response = fetch("https://kitt.lewagon.com/api/v1/users?page=#{@page_num}")
-      data = JSON.parse(response.read_body)
+      data = JSON.parse(fetch("https://kitt.lewagon.com/api/v1/users?page=#{page}").read_body)
+      Rails.logger.info("Scraping page #{@page}/#{data['page_count']}...")
 
       push_new_users(data["users"])
 
-      @page_num += 1
+      page += 1
       break unless data["has_more"]
     end
 
     append_new_users_to_csv
   end
 
-  def push_new_users(users)
-    users.each do |user|
-      user = user["alumnus"]
-      uid = user["id"]
-      next if @uids.include?(uid.to_s)
+  def perform_upsert_data
+    cities = Hash.new { |city_hash, city| city_hash[city] = Hash.new(0) }
+    CSV.foreach(CSV_PATH, headers: true) { |row| cities[row["city_name"]][row["batch_number"]] += 1 }
 
-      batch_number = user["camp_slug"]
-      next unless batch_number.to_i > 0
+    cities.each do |city_name, batches|
+      city = City.find_or_initialize_by(name: city_name)
+      city.update!(size: batches.values.sum)
 
-      city_name = user["city"]
-      batch_year = "20#{user['camp_date'][-2..]}"
-      @new_users << [uid, batch_number, batch_year, city_name]
+      batches.each do |batch_number, size|
+        batch = Batch.find_or_initialize_by(number: batch_number)
+        batch.update!(size:, city:)
+      end
     end
   end
 
-  # CityOfUsers: Array<[city_name, Array<User>]>
-  # CityOfBatches: Array<[city_name, city_size, Array<[batch_number, Array<User>]>]>
-  def perform_update_batches
-    @users
-      .group_by { |user| user[3] }.entries # CityOfUsers
-      .map { |city_name, users| [city_name, users.length, users.group_by { |user| user[1] }.entries] } # CityOfBatches
-      .each do |city_name, city_size, batches|
-        city = City.find_or_initialize_by(name: city_name)
-        city.update!(size: city_size)
+  def push_new_users(users)
+    users.each do |user|
+      alumnus = user["alumnus"]
+      uid = alumnus["id"]
+      next if @known_uids.include?(uid)
 
-        batches.each do |batch_number, users|
-          batch = Batch.find_or_initialize_by(number: batch_number)
-          batch.update!(size: users.length, city:)
-        end
-      end
+      batch_number = alumnus["camp_slug"].to_i
+      next if batch_number == 0
+
+      city_name = alumnus["city"]
+      batch_year = Date.parse(alumnus["camp_date"]).year
+      @new_users << [uid, batch_number, batch_year, city_name]
+    end
   end
 end

e1f4fbd7286fca18133367efc56e51904e2a0d0d
Pilou <8350914+pil0u@users.noreply.github.com>
2023-11-10 10:37:47 +0100

    Double quotes everywhere (#311)
    * Double quotes everywhere
    
    * Encore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is copied to spec/ when you run "rails generate rspec:install"
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)

ec9cfb56eae8e71fc8bde72d7d699d81d7b86538
pil0u <8350914+pil0u@users.noreply.github.com>
2023-11-12 11:40:25 +0100

    Rollback formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  skip_before_action  :authenticate_user!,  only: %i[admin code_of_conduct faq participation       stats welcome]
-  before_action       :render_countdown,    only: %i[      code_of_conduct faq participation setup stats welcome], if: :render_countdown?
+  skip_before_action :authenticate_user!, only: %i[admin code_of_conduct faq participation stats welcome]
+  before_action      :render_countdown,   only: %i[code_of_conduct faq participation setup stats welcome], if: :render_countdown?
 
   def admin; end
 

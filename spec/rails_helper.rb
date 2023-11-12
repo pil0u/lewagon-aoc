c2ac72b423b38d1ca43a5555277f0a5862704d38
Jérémie <jeremie.bonal@gmail.com>
2023-11-12 10:33:47 +0100

    Add spec for unlinking

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,8 @@ end
 RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
 
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join("/spec/fixtures")
 

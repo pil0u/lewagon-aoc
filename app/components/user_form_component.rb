439f48d8d4584c2e8258dd09c5946674590fde03
Louis Ramos <louisramosdev@gmail.com>
2023-11-19 20:04:35 +0100

    refactor(users): set awareness_options as constant

--- app/components/user_form_component.rb
+++ app/components/user_form_component.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 class UserFormComponent < ApplicationComponent
+  AWARENESS_OPTIONS = {
+    slack_aoc: "Slack channel #aoc",
+    slack_general: "Slack channel #general",
+    slack_campus: "Slack channel of your campus",
+    slack_batch: "Slack channel of your batch",
+    newsletter: "Email newsletter",
+    linkedin: "Linkedin",
+    facebook: "Facebook",
+    instagram: "Instagram",
+    brussels_event: "Launch party in Brussels",
+    london_event: "Testing workshop in London"
+  }.freeze
+
   def initialize(user:)
     @user = user
-    @awareness_options = {
-      slack_aoc: "Slack channel #aoc",
-      slack_general: "Slack channel #general",
-      slack_campus: "Slack channel of your campus",
-      slack_batch: "Slack channel of your batch",
-      newsletter: "Email newsletter",
-      linkedin: "Linkedin",
-      facebook: "Facebook",
-      instagram: "Instagram",
-      brussels_event: "Launch party in Brussels",
-      london_event: "Testing workshop in London"
-    }
   end
 end

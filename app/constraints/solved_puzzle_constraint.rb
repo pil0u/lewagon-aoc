8f254367e748cfe5836f7a381d2a3ba83222bc3d
Louis Ramos <louisramosdev@gmail.com>
2023-11-19 13:15:30 +0100

    feat(snippets): unlock solutions for everyone after 24 hours

--- app/constraints/solved_puzzle_constraint.rb
+++ app/constraints/solved_puzzle_constraint.rb
@@ -2,6 +2,8 @@
 
 class SolvedPuzzleConstraint
   def matches?(request)
+    return true if DateTime.current > Aoc.begin_time.change(day: request.params[:day] + 1)
+
     request.env["warden"]&.user&.solved?(request.params[:day], request.params[:challenge])
   end
 end

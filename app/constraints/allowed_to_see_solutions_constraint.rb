705685a2667febc4c3ac6e75d84f6acb9862b518
Louis Ramos <75388869+wJoenn@users.noreply.github.com>
2023-11-21 11:09:30 +0100

    feat(snippets): unlock solutions for everyone after 24 hours (#336)
    * feat(snippets): unlock solutions for everyone after 24 hours
    
    * refactor(snippets): rename SolvedPuzzleConstraint to AllowedToSeeSolutionsConstraint
    
    * Change to Time.now.utc, just for the sake of consistency
    
    * Consistent syntax with the release_time in days controller
    
    * Empty commit
    
    * Empty commit (#343)
    
    ---------
    
    Co-authored-by: pil0u <8350914+pil0u@users.noreply.github.com>

--- app/constraints/allowed_to_see_solutions_constraint.rb
+++ app/constraints/allowed_to_see_solutions_constraint.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class SolvedPuzzleConstraint
+class AllowedToSeeSolutionsConstraint
   def matches?(request)
+    return true if Time.now.utc > Aoc.begin_time + (request.params[:day] + 1).days
+
     request.env["warden"]&.user&.solved?(request.params[:day], request.params[:challenge])
   end
 end

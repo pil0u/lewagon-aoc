d215576fb8d3fcbf2ed5251961dcc6961d160808
pil0u <8350914+pil0u@users.noreply.github.com>
2023-11-21 10:44:45 +0100

    Change to Time.now.utc, just for the sake of consistency

--- app/constraints/allowed_to_see_solutions_constraint.rb
+++ app/constraints/allowed_to_see_solutions_constraint.rb
@@ -2,7 +2,7 @@
 
 class AllowedToSeeSolutionsConstraint
   def matches?(request)
-    return true if DateTime.current > Aoc.begin_time.change(day: request.params[:day] + 1)
+    return true if Time.now.utc > Aoc.begin_time.change(day: request.params[:day] + 1)
 
     request.env["warden"]&.user&.solved?(request.params[:day], request.params[:challenge])
   end

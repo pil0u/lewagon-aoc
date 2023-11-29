7b626ed87a2c2bada852eeb22e665bf345ef4e33
pil0u <8350914+pil0u@users.noreply.github.com>
2023-11-29 22:09:01 +0100

    Update sizes of cities based on Seb's export (add Belize)

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,65 +16,66 @@ if Rails.env.development?
 end
 
 campuses = [
-  { name: "Amsterdam",      size: 666 },
-  { name: "B2G Latam",      size: 105 },
-  { name: "Bali",           size: 421 },
-  { name: "Barcelona",      size: 610 },
-  { name: "Beirut",         size: 14 },
-  { name: "Belo Horizonte", size: 22 },
-  { name: "Berlin",         size: 2563 },
-  { name: "Bordeaux",       size: 712 },
-  { name: "Brasilia",       size: 134 },
-  { name: "Brussels",       size: 837 },
-  { name: "Buenos Aires",   size: 397 },
+  { name: "Amsterdam",      size: 668 },
+  { name: "B2G Latam",      size: 105 },  # not in export
+  { name: "Bali",           size: 423 },
+  { name: "Barcelona",      size: 611 },
+  { name: "Beirut",         size: 14 },  # not in export
+  { name: "Belize",         size: 139 },
+  { name: "Belo Horizonte", size: 22 },  # not in export
+  { name: "Berlin",         size: 2598 },
+  { name: "Bordeaux",       size: 713 },
+  { name: "Brasilia",       size: 134 },  # not in export
+  { name: "Brussels",       size: 838 },
+  { name: "Buenos Aires",   size: 419 },
   { name: "Cape Town",      size: 92 },
-  { name: "Casablanca",     size: 143 },
+  { name: "Casablanca",     size: 163 },
   { name: "Chengdu",        size: 64 },
-  { name: "Cologne",        size: 97 },
-  { name: "Copenhagen",     size: 117 },
-  { name: "Dubai",          size: 35 },
-  { name: "Emil",           size: 172 },
+  { name: "Cologne",        size: 98 },
+  { name: "Copenhagen",     size: 117 },  # not in export
+  { name: "Dubai",          size: 35 },  # not in export
+  { name: "Emil",           size: 173 },
   { name: "Essonne",        size: 16 },
-  { name: "For Business",   size: 217 },
-  { name: "Istanbul",       size: 30 },
-  { name: "Kyoto",          size: 18 },
-  { name: "Lausanne",       size: 191 },
+  { name: "For Business",   size: 219 },
+  { name: "Istanbul",       size: 30 },  # not in export
+  { name: "Kyoto",          size: 18 },  # not in export
+  { name: "Lausanne",       size: 204 },
   { name: "Lille",          size: 484 },
-  { name: "Lima",           size: 53 },
-  { name: "Lisbon",         size: 1037 },
-  { name: "London",         size: 2366 },
-  { name: "Lyon",           size: 575 },
-  { name: "Madrid",         size: 209 },
-  { name: "Malmö",          size: 21 },
-  { name: "Marseille",      size: 687 },
-  { name: "Martinique",     size: 42 },
-  { name: "Mauritius",      size: 184 },
-  { name: "Medellín",       size: 71 },
-  { name: "Melbourne",      size: 468 },
-  { name: "Mexico",         size: 301 },
-  { name: "Milan",          size: 137 },
-  { name: "Montréal",       size: 645 },
-  { name: "Munich",         size: 293 },
+  { name: "Lima",           size: 53 },  # not in export
+  { name: "Lisbon",         size: 1040 },
+  { name: "London",         size: 2427 },
+  { name: "Lyon",           size: 576 },
+  { name: "Madrid",         size: 211 },
+  { name: "Malmö",          size: 21 },  # not in export
+  { name: "Marseille",      size: 729 },
+  { name: "Martinique",     size: 55 },
+  { name: "Mauritius",      size: 199 },
+  { name: "Medellín",       size: 71 },  # not in export
+  { name: "Melbourne",      size: 537 },
+  { name: "Mexico",         size: 315 },
+  { name: "Milan",          size: 137 },  # not in export
+  { name: "Montréal",       size: 655 },
+  { name: "Munich",         size: 294 },
   { name: "Nantes",         size: 375 },
-  { name: "Nice",           size: 200 },
-  { name: "Online",         size: 1696 },
-  { name: "Oslo",           size: 55 },
-  { name: "Paris",          size: 4189 },
+  { name: "Nice",           size: 209 },
+  { name: "Online",         size: 1861 },
+  { name: "Oslo",           size: 55 },  # not in export
+  { name: "Paris",          size: 4347 },
   { name: "Porto",          size: 53 },
   { name: "Rennes",         size: 77 },
   { name: "Rio de Janeiro", size: 488 },
   { name: "Riyadh",         size: 74 },
   { name: "Santiago",       size: 98 },
   { name: "Seine et Marne", size: 25 },
-  { name: "Seoul",          size: 16 },
-  { name: "Shanghai",       size: 435 },
-  { name: "Shenzhen",       size: 24 },
-  { name: "Singapore",      size: 402 },
-  { name: "Stockholm",      size: 51 },
+  { name: "Seoul",          size: 16 },  # not in export
+  { name: "Shanghai",       size: 453 },
+  { name: "Shenzhen",       size: 24 },  # not in export
+  { name: "Singapore",      size: 428 },
+  { name: "Stockholm",      size: 51 },  # not in export
   { name: "Sydney",         size: 2 },
-  { name: "São Paulo",      size: 1043 },
-  { name: "Tel Aviv",       size: 158 },
-  { name: "Tokyo",          size: 816 },
+  { name: "São Paulo",      size: 1097 },
+  { name: "Tel Aviv",       size: 158 },  # not in export
+  { name: "Tokyo",          size: 879 },
   { name: "Toulouse",       size: 20 },
   { name: "Zurich",         size: 64 }
 ]

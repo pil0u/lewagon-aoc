2c87bde7b7d5bb01bdb856ff0e86acbb63185e35
Louis Ramos <louisramosdev@gmail.com>
2023-11-30 21:07:46 +0100

    feat(snippets): create ReactionsController

---
+++ app/controllers/reactions_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class ReactionsController < ApplicationController
+  before_action :set_reaction, only: %i[update destroy]
+
+  def create
+    reaction = Reaction.new(reaction_params)
+    reaction.snippet = Snippet.find(params[:snippet_id])
+    reaction.user = current_user
+
+    if reaction.save
+      redirect_back fallback_location: "/"
+    else
+      redirect_back fallback_location: "/", alert: reaction.errors.full_messages[0].to_s
+    end
+  end
+
+  def update
+    if @reaction.update(reaction_params)
+      redirect_back fallback_location: "/"
+    else
+      redirect_back fallback_location: "/", alert: reaction.errors.full_messages[0].to_s
+    end
+  end
+
+  def destroy
+    @reaction.destroy
+    redirect_back fallback_location: "/"
+  end
+
+  private
+
+  def set_reaction
+    @reaction = current_user.reactions.find(params[:id])
+  end
+
+  def reaction_params
+    params.require(:reaction).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -56,29 +56,32 @@ Rails.application.routes.draw do
 
   # Routes for authenticated + confirmed users
   authenticated :user, ->(user) { user.confirmed? } do
-    get     "/",                    to: "pages#calendar", as: :calendar
-    get     "/countdown",           to: "pages#countdown"
-    get     "/patrons",             to: "pages#patrons"
-    get     "/campus/:slug",        to: "campuses#show",  as: :campus
-    get     "/city/:slug",          to: "campuses#show",  as: :city # Retrocompat in case of old links
-    get     "/day/:day",            to: "days#show",      as: :day,     day: /[1-9]|1\d|2[0-5]/
-    get     "/day/:day/:challenge", to: "snippets#show",  as: :snippet, day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
-    post    "/day/:day/:challenge", to: "snippets#create",              day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
-    get     "/the-wall",            to: "messages#index", as: :messages
-    post    "/the-wall",            to: "messages#create"
-    get     "/scores/campuses",     to: "scores#campuses"
-    get     "/scores/cities",       to: "scores#campuses" # Retrocompat in case of old links
-    get     "/scores/insanity",     to: "scores#insanity"
-    get     "/scores/solo",         to: "scores#solo"
-    get     "/scores/squads",       to: "scores#squads"
-    get     "/squad/:id",           to: "squads#show",      as: :squad
-    post    "/squad",               to: "squads#create",    as: :create_squad
-    patch   "/squad/:id",           to: "squads#update",    as: :update_squad
-    post    "/squad/join",          to: "squads#join",      as: :join_squad
-    delete  "/squad/leave",         to: "squads#leave",     as: :leave_squad
-    get     "/profile/:uid",        to: "users#show",       as: :profile
-    get     "/settings",            to: "users#edit"
-    patch   "/settings",            to: "users#update"
+    get     "/",                                to: "pages#calendar", as: :calendar
+    get     "/countdown",                       to: "pages#countdown"
+    get     "/patrons",                         to: "pages#patrons"
+    get     "/campus/:slug",                    to: "campuses#show",  as: :campus
+    get     "/city/:slug",                      to: "campuses#show",  as: :city # Retrocompat in case of old links
+    get     "/day/:day",                        to: "days#show",      as: :day,     day: /[1-9]|1\d|2[0-5]/
+    get     "/day/:day/:challenge",             to: "snippets#show",  as: :snippet, day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/ # , constraints: AllowedToSeeSolutionsConstraint.new
+    post    "/day/:day/:challenge",             to: "snippets#create",              day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/ # , constraints: AllowedToSeeSolutionsConstraint.new
+    get     "/the-wall",                        to: "messages#index", as: :messages
+    post    "/the-wall",                        to: "messages#create"
+    get     "/scores/campuses",                 to: "scores#campuses"
+    get     "/scores/cities",                   to: "scores#campuses" # Retrocompat in case of old links
+    get     "/scores/insanity",                 to: "scores#insanity"
+    get     "/scores/solo",                     to: "scores#solo"
+    get     "/scores/squads",                   to: "scores#squads"
+    get     "/squad/:id",                       to: "squads#show",      as: :squad
+    post    "/squad",                           to: "squads#create",    as: :create_squad
+    patch   "/squad/:id",                       to: "squads#update",    as: :update_squad
+    post    "/squad/join",                      to: "squads#join",      as: :join_squad
+    delete  "/squad/leave",                     to: "squads#leave",     as: :leave_squad
+    get     "/profile/:uid",                    to: "users#show",       as: :profile
+    get     "/settings",                        to: "users#edit"
+    patch   "/settings",                        to: "users#update"
+    post    "/snippets/:snippet_id/reactions",  to: "reactions#create",  as: :reactions # ,      #   constraints: AllowedToSeeSolutionsConstraint.new
+    patch   "/reactions/:id",                   to: "reactions#update",  as: :update_reaction # , constraints: AllowedToSeeSolutionsConstraint.new
+    delete  "/reactions/:id",                   to: "reactions#destroy", as: :delete_reaction # ,                      constraints: AllowedToSeeSolutionsConstraint.new
   end
 
   # Admin routes

80f812b913b676c7b39749c62042cf35635eb931
pil0u <8350914+pil0u@users.noreply.github.com>
2023-12-02 00:24:30 +0100

    Wording

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -7,16 +7,16 @@ Rails.application.configure do
     shutdown_timeout: 30,
     enable_cron: true,
     cron: {
-      refresh_completions: {
-        cron: "*/10 * 1-30 11-12 *", # every 10 minutes between November 1st and December 30th
+      refresh_completions: { # every 10 minutes between November 1st and December 30th
+        cron: "*/10 * 1-30 11-12 *",
         class: "InsertNewCompletionsJob"
       },
-      auto_cleanup: {
-        cron: "55 23 * * * on America/New_York", # every day before a new puzzle
+      auto_cleanup: { # every day before a new puzzle
+        cron: "55 23 * * * on America/New_York",
         class: "Cache::CleanupJob"
       },
-      lock_time_achievements: {
-        cron: "30 17 8 12 * Europe/Paris", # on lock tim
+      unlock_lock_time_achievements: {
+        cron: "30 17 8 12 * Europe/Paris",
         class: "Achievements::LockTimeJob"
       }
     }

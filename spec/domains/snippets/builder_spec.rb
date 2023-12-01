203bfa116ba8a171a1d035c07dd6406900437217
Jérémie <jeremie.bonal@gmail.com>
2023-12-01 13:08:09 +0100

    Add basic spec for Snippet builder

---
+++ spec/domains/snippets/builder_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Snippets::Builder do
+
+  let(:language) { :markdown }
+  let(:code) { <<-MARKDOWN }
+    Hello ! This is *my* solution !
+  MARKDOWN
+
+  it "builds a snippet with the provided attributes" do
+    result = described_class.call(code: code, language: language)
+    expect(result).to be_a Snippet
+    expect(result.attributes.symbolize_keys).to include(code: code, language: 'markdown')
+  end
+
+  context "when submitted straight code" do
+    let(:language) { :ruby }
+    let(:code) { <<-RUBY }
+      puts "My solution"
+    RUBY
+
+    it "wraps the code in a fenced block of specified language" do
+      result = described_class.call(code: code, language: language)
+      expect(result).to be_a Snippet
+      expect(result.attributes.symbolize_keys).to include(code: "```ruby\n#{code}\n```\n", language: 'ruby')
+    end
+  end
+end

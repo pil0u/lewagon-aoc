95dff46644bc7aafc62807411bb197a60ad3be7f
Louis Ramos <louisramosdev@gmail.com>
2023-12-01 07:39:11 +0100

    fix(snippets): add missing params to reactions

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -4,7 +4,7 @@ class ReactionsController < ApplicationController
   before_action :set_reaction, only: %i[update destroy]
 
   def create
-    reaction = current_user.reactions.build(snippet_id: params[:snippet_id])
+    reaction = current_user.reactions.build(reaction_params.merge(snippet_id: params[:snippet_id]))
 
     if reaction.save
       redirect_back fallback_location: "/"

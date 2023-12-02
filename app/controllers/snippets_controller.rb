02eedb7a2de851a5e12f35f93e8c07a716f15198
Louis Ramos <louisramosdev@gmail.com>
2023-12-02 17:41:55 +0100

    feat(snippets): create edit and update actions for SnippetsController

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SnippetsController < ApplicationController
+  before_action :set_snippet, only: %i[edit create]
+
   def show
     @day = params[:day]
     @challenge = params[:challenge]
@@ -14,6 +16,13 @@ class SnippetsController < ApplicationController
       Write a super nice guide in Markdown, it will work too.
       If your Markdown tutorial features Python code, choose Python as a language.
     TEXT
+  end
+
+  def edit
+    @day = params[:day]
+    @challenge = params[:challenge]
+
+    @snippets = Snippet.includes(:user, :reactions).where(day: @day, challenge: @challenge).order(created_at: :desc)
   end
 
   def create
@@ -32,7 +41,19 @@ class SnippetsController < ApplicationController
     end
   end
 
+  def update
+    if @snippet.update(snippet_params)
+      redirect_to snippet_path(day: params[:day], challenge: params[:challenge]), notice: "Your solution was edited"
+    else
+      redirect_to snippet_path(day: params[:day], challenge: params[:challenge]), alert: @snippet.errors.full_messages[0].to_s
+    end
+  end
+
   private
+
+  def set_snippet
+    @snippet = current_user.snippets.find(params[:id])
+  end
 
   def snippet_params
     params.require(:snippet).permit(:code, :language)

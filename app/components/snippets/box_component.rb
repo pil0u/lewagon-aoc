546f623c39b22ee169d7280534bf28c651c242b8
Louis Ramos <louisramosdev@gmail.com>
2023-12-03 15:45:20 +0100

    refactor(snippets): move edit button condition inside Class

--- app/components/snippets/box_component.rb
+++ app/components/snippets/box_component.rb
@@ -9,6 +9,8 @@ module Snippets
     def initialize(snippet:, user:)
       @user = user
       @snippet = snippet
+
+      @user_can_edit_snippet = @snippet.user == @user && Time.now.utc < Aoc.release_time(@snippet.day) + 48.hours
     end
   end
 end

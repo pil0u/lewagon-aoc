fa339a0e0199f5cfba69ea82615292e909e33fca
Victor Holl <v_h@me.com>
2023-12-11 11:22:53 +0100

    use colorize_hardcore logic in daily

--- app/components/scores/daily_user_row_component.rb
+++ app/components/scores/daily_user_row_component.rb
@@ -6,9 +6,10 @@ module Scores
 
     with_collection_parameter :participant
 
-    def initialize(participant:, user:)
+    def initialize(participant:, user:, colorize_hardcore: false)
       @participant = participant
       @user = user
+      @colorize_hardcore = colorize_hardcore
     end
   end
 end

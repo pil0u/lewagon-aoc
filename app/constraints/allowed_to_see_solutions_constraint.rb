d9b7a59a1f15abff5b9f5c9c7807d2603e49b4d0
pil0u <8350914+pil0u@users.noreply.github.com>
2024-10-03 15:35:24 +0200

    ...or when they solved it themselves

--- app/constraints/allowed_to_see_solutions_constraint.rb
+++ app/constraints/allowed_to_see_solutions_constraint.rb
@@ -2,7 +2,20 @@
 
 class AllowedToSeeSolutionsConstraint
   def matches?(request)
-    Completion.where(day: request.params[:day], challenge: request.params[:challenge])
-              .count >= 5
+    @day = request.params[:day]
+    @challenge = request.params[:challenge]
+    @user = request.env["warden"]&.user
+
+    solved_by_current_user? || solved_by_five_users?
+  end
+
+  private
+
+  def solved_by_current_user?
+    @user&.solved?(@day, @challenge)
+  end
+
+  def solved_by_five_users?
+    Completion.where(day: @day, challenge: @challenge).count >= 5
   end
 end

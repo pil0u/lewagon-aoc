e62b9e34d8cc7573f569e646ff84a6a818d11bda
pil0u <8350914+pil0u@users.noreply.github.com>
2024-11-03 18:16:21 +0100

    Add a Stimulus controller for 'read more' feature on snippets

---
+++ app/javascript/controllers/show_more_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["codeBlock", "button"]
+
+    connect() {
+        // Hide the button if the code block is not truncated
+        if (this.codeBlockTarget.scrollHeight <= this.codeBlockTarget.offsetHeight) {
+            this.buttonTarget.classList.add("hidden")
+        }
+    }
+
+    toggle() {
+        const isExpanded = !this.codeBlockTarget.classList.toggle("truncated")
+        this.buttonTarget.textContent = isExpanded ? "↑↑↑" : "↓↓↓"
+    }
+}

a429e54d7599e693291e3ef722a90b2027f65c84
pil0u <8350914+pil0u@users.noreply.github.com>
2024-11-04 21:03:20 +0100

    Refine the event_awareness enum

--- app/components/users/account_settings_component.rb
+++ app/components/users/account_settings_component.rb
@@ -3,11 +3,13 @@
 module Users
   class AccountSettingsComponent < ApplicationComponent
     AWARENESS_OPTIONS = {
+      newsletter: "Email newsletter",
       slack_aoc: "Channel #aoc on Slack",
       slack_general: "Channel #general on Slack",
+      slack_payforward: "Channel #pay-forward on Slack",
       slack_campus: "Your campus channel on Slack",
       slack_batch: "Your batch channel on Slack",
-      newsletter: "Email newsletter"
+      other: "Other"
     }.freeze
 
     def initialize(user:)

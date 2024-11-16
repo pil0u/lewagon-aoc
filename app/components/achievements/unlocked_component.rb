a988c6c16816f1d944b4ff47ff5a65740f2b0f94
pil0u <8350914+pil0u@users.noreply.github.com>
2024-11-16 12:50:44 +0100

    Revamp the achievement view component to take nature, state and title parameters based on 3 different states: locked, unlocked, unlocked and current_user has unlocked it

--- app/components/achievements/unlocked_component.rb
+++ app/components/achievements/unlocked_component.rb
@@ -2,10 +2,27 @@
 
 module Achievements
   class UnlockedComponent < ApplicationComponent
-    with_collection_parameter :nature
+    def initialize(nature:, state:, title:)
+      @nature, @title, @state_css = {
+        locked: ["surprise", random_locked_message, "achievement-locked"],
+        unlocked: [nature, title, "achievement-unlocked"],
+        unlocked_plus: [nature, title, "achievement-unlocked-plus"]
+      }[state]
+    end
 
-    def initialize(nature:)
-      @nature = nature
+    private
+
+    def random_locked_message
+      [
+        "Click to reveal! Nah, just kidding.",
+        "Soon™",
+        "Do you really wanna know what's behind this?",
+        "Nothing to see here... yet!",
+        "This achievement is shy and doesn't want to come out",
+        "Keep trying! Or don't. I'm just a tooltip.",
+        "Legend says this achievement exists...",
+        "The achievement is in another castle!"
+      ].sample
     end
   end
 end

42be9996b8b7b9cd7344c1dc234bcc6cf6515430
pil0u <8350914+pil0u@users.noreply.github.com>
2024-11-18 00:52:49 +0100

    Clean up

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -3,8 +3,6 @@
 class Achievement < ApplicationRecord
   belongs_to :user
 
-  # Define a scope for each achievement type
-  # edition2020 edition2021 countdown_riddle setup_complete city_join full_squad sanity insanity stars1 stars5 stars11 stars23 speed10 speed30 speed90 speed180].each do |nature|
   %i[fan jedi_master madness].each do |nature|
     scope nature, -> { where(nature: nature.to_s) }
   end

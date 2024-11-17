008ca871398e6fb7a01a3a226c63b2a016200d6f
pil0u <8350914+pil0u@users.noreply.github.com>
2024-11-17 23:52:56 +0100

    Remove unnecessary parameters and constraint to the snippets routes

--- config/routes.rb
+++ config/routes.rb
@@ -64,8 +64,8 @@ Rails.application.routes.draw do
     get     "/day/:day",                        to: "days#show",       as: :day,            day: /[1-9]|1\d|2[0-5]/
     get     "/day/:day/:challenge",             to: "snippets#show",   as: :snippet,        day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
     post    "/day/:day/:challenge",             to: "snippets#create",                      day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
-    get     "/snippets/:id/edit",               to: "snippets#edit",   as: :edit_snippet,   day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
-    patch   "/snippets/:id",                    to: "snippets#update", as: :update_snippet, day: /[1-9]|1\d|2[0-5]/, challenge: /[1-2]/, constraints: AllowedToSeeSolutionsConstraint.new
+    get     "/snippets/:id/edit",               to: "snippets#edit",   as: :edit_snippet
+    patch   "/snippets/:id",                    to: "snippets#update", as: :update_snippet
     get     "/the-wall",                        to: "messages#index",  as: :messages
     post    "/the-wall",                        to: "messages#create"
     get     "/scores/squads",                   to: "scores#squads",    as: :scores_squads

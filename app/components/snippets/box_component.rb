3aa8d643f619f1790fca1e324648d537f6c2b985
Louis Ramos <louisramosdev@gmail.com>
2024-11-26 22:35:51 +0100

    refactor(snippets): use slack_linkeD?

--- app/components/snippets/box_component.rb
+++ app/components/snippets/box_component.rb
@@ -11,7 +11,7 @@ module Snippets
       @snippet = snippet
 
       @user_can_edit_snippet = @snippet.user == @user && Time.now.utc < Aoc.release_time(@snippet.day) + 48.hours
-      @user_can_discuss_snippet = @snippet.user != @user && @snippet.user.slack_id.present?
+      @user_can_discuss_snippet = @snippet.user != @user && @snippet.user.slack_linked?
     end
   end
 end

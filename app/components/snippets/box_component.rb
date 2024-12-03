fe1dffbda80fa50e52fff5985451ed9818c36e22
Louis Ramos <louisramosdev@gmail.com>
2024-12-03 22:02:48 +0100

    feat(snippets): require slack_linked? to discuss

--- app/components/snippets/box_component.rb
+++ app/components/snippets/box_component.rb
@@ -11,7 +11,7 @@ module Snippets
       @snippet = snippet
 
       @user_can_edit_snippet = @snippet.user == @user
-      @user_can_discuss_snippet = @snippet.user != @user && @snippet.user.slack_linked?
+      @user_can_discuss_snippet = @snippet.user != @user && @snippet.user.slack_linked? && @user.slack_linked?
       @user_reaction_vote_value = @snippet.reactions.find { _1.user_id == @user.id }
 
       if @snippet.slack_url.nil?

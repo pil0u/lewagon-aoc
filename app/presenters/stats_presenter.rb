f03f383b802e96e86bc16dd3257f9b6c4cbfc251
Pilou <8350914+pil0u@users.noreply.github.com>
2025-11-29 16:59:30 +0100

    Revive 2025 - Additional fixes (#761)
    * change hardcoded dates from 25th to 12th
    
    * specify to create new completions after first user created
    
    * fix Rails version in README
    
    * update bundler
    
    * don't grant mobster achievement if there is no squad
    
    * strike the 25th on the welcome page
    
    * change link to setup support thread

--- app/presenters/stats_presenter.rb
+++ app/presenters/stats_presenter.rb
@@ -91,7 +91,7 @@ class StatsPresenter # rubocop:disable Metrics/ClassLength
 
   def set_mobster_achievement
     biggest_squad_id = Squad.joins(:users).group(:id).order("COUNT(users.id) DESC, created_at").first&.id
-    user_is_in_biggest_squad = @user&.squad_id == biggest_squad_id
+    user_is_in_biggest_squad = biggest_squad_id.present? && @user&.squad_id == biggest_squad_id
 
     state = :locked
     state = :unlocked_plus if user_is_in_biggest_squad
